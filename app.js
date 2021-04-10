d0858c11d5ddab859c8d9d08e59a5e917d337414
startsource <startnexus@gmail.com>
2021-04-10 12:16:57 +0900

    3rd commit

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas")
+const ctx = canvas.getContext("2d");
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+ctx.storkeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,23 +13,29 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.storkeStyle();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting()
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove)
-  canvas.addEventListener("mousedown", onMouseDown)
-  canvas.addEventListener("mouseup", onMouseUp)
+  canvas.addEventListener("mousedown", startPainting)
+  canvas.addEventListener("mouseup", stopPainting)
   canvas.addEventListener("mouseleave", stopPainting)
 }
